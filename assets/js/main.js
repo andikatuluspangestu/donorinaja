219518be1131378a2da17ffef04fadb97856610e
andikatuluspangestu <andhikatuluspangestu@gmail.com>
2024-08-30 22:14:55 +0700

    update: Refactor to ES6
    Signed-off-by: andikatuluspangestu <andhikatuluspangestu@gmail.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,30 @@
-window.onscroll = function () {
-  var navbar = document.querySelector('.navbar');
-  if (window.pageYOffset > 50) { // Threshold of 50px scroll
-    navbar.classList.add('sticky');
-  } else {
-    navbar.classList.remove('sticky');
-  }
+window.onscroll = () => {
+  const navbar = document.querySelector('.navbar');
+
+  window.pageYOffset > 50
+    ? navbar.classList.add('sticky')
+    : navbar.classList.remove('sticky');
+
 };
+
+AOS.init({
+  duration: 1000,
+  offset: 120,
+  once: true,
+  disable: () => window.innerWidth < 768,
+  mirror: true,
+  delay: 100,
+  easing: 'ease-in-out',
+  anchorPlacement: 'top-bottom',
+});
+
+// Konfigurasi agar ketika gulir scrollbar halaman ke bawah, akan terasa halus dan tidak terlalu cepat
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+});
