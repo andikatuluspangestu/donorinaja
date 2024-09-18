ee52489d6811e8bb13053facf7cdbe2f4e5f64c3
andikatuluspangestu <andhikatuluspangestu@gmail.com>
2024-09-18 12:12:40 +0700

    Refactor vite.config.js to simplify build configuration and add input options for multiple pages
    Signed-off-by: andikatuluspangestu <andhikatuluspangestu@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,27 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: './src', // Menggunakan folder src sebagai root
+  root: './src',
   build: {
-    outDir: '../dist', // Output akan ke folder dist
-    emptyOutDir: true, // Kosongkan folder dist setiap build
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: './src/index.html',
+        about: './src/pages/about.html',
+        blood_stock: './src/pages/blood_stock.html',
+        donor_events: './src/pages/donor_events.html',
+        donor_history: './src/pages/donor_history.html',
+        donor_steps: './src/pages/donor_steps.html',
+        event_detail: './src/pages/event_detail.html',
+        login: './src/pages/login.html',
+        privacy: './src/pages/privacy.html',
+        profile: './src/pages/profile.html',
+        register: './src/pages/register.html',
+      }
+    }
   },
   server: {
-    port: 9000, // Port untuk development server
+    port: 9000,
   },
 });
